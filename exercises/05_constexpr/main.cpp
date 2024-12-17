c7d4642c36a670380aa527687168ffc84c289686
xiaoyuTT <2309224467@qq.com>
2024-12-18 00:47:53 +0800

    00-06

--- exercises/05_constexpr/main.cpp
+++ exercises/05_constexpr/main.cpp
@@ -17,8 +17,8 @@ int main(int argc, char **argv) {
     std::cout << "fibonacci(20) = " << FIB20 << std::endl;
 
     // TODO: 观察错误信息，修改一处，使代码编译运行
-    // PS: 编译运行，但是不一定能算出结果……
-    constexpr auto ANS_N = 90;
+    // PS: 编译运行，但是不一定能算出结果……因为计算两级太大，即去除ANS的constexpr声明
+    constexpr auto ANS_N = 9;
     constexpr auto ANS = fibonacci(ANS_N);
     std::cout << "fibonacci(" << ANS_N << ") = " << ANS << std::endl;
 
